57a92f32e9bec1f0938b3cbdac8971b82654245a
YuriRafael10 <yurilana1001@gmail.com>
2024-01-22 12:28:45 -0300

    Creating the repository for User

---
+++ src/main/java/com/yurirafael/todosimple/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yurirafael.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yurirafael.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
